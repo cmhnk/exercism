024106e32d18850035af6e098728bd1695b1e29f
cmhnk <corinne@automatedinsights.com>
2017-07-31 17:04:56 -0400

    Try to fix spacing issue in Sublime...

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,17 +1,1 @@
-class Hamming 
-
-  def self.compute(strand1, strand2)
-  	# raise an error if strand1.size != strand2.size
-  	return 0 if strand1 == strand2
-  	find_unequal_indexes(strand1, strand2)
-  end
-
-  def self.find_unequal_indexes(strand1, strand2)
-  	unequal_index_count = 0
-  	strand1.chars.map.with_index do |char, i|
-  	  next if char == strand2[i]
-  	  unequal_index_count += 1
-  	end
-  	unequal_index_count 
-  end
-end
+class Hamming def self.compute(strand1, strand2)  raise ArgumentError if strand1.size != strand2.size  find_unequal_indexes(strand1, strand2) end def self.find_unequal_indexes(strand1, strand2)  unequal_index_count = 0  strand1.chars.map.with_index do |char, i|    unequal_index_count += 1 unless char == strand2[i]  end  unequal_index_count endendmodule BookKeeping VERSION = 3end
