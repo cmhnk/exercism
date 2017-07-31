6001067daee178ce86dcbd1456cd5fcbffcc9188
cmhnk <corinne@automatedinsights.com>
2017-07-31 17:06:37 -0400

    Fix spacing

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,18 +1,18 @@
 class Hamming
- def self.compute(strand1, strand2)
-  raise ArgumentError if strand1.size != strand2.size
-  find_unequal_indexes(strand1, strand2)
- end
-
- def self.find_unequal_indexes(strand1, strand2)
-  unequal_index_count = 0
-  strand1.chars.map.with_index do |char, i| 
-   unequal_index_count += 1 unless char == strand2[i]
+  def self.compute(strand1, strand2)
+    raise ArgumentError if strand1.size != strand2.size
+    find_unequal_indexes(strand1, strand2)
   end
-  unequal_index_count
- end
+
+  def self.find_unequal_indexes(strand1, strand2)
+   unequal_index_count = 0
+   strand1.chars.map.with_index do |char, i| 
+    unequal_index_count += 1 unless char == strand2[i]
+   end
+   unequal_index_count
+  end
 end
 
 module BookKeeping
- VERSION = 3
+  VERSION = 3
 end
